6953e8ed1503a8fa4c6f457c44582507b834eed6
antonioEpifani <githubReplyAntonio96>
2021-11-17 13:17:17 +0100

    Added purge method, added progress bar

--- helper.py
+++ helper.py
@@ -1,14 +1,15 @@
 import os
 import re
 import subprocess as sp
+from progressPrint import printProgressBar
 
-class Parser:
-    def __init__(self, filename):
-        self.filename = filename
+class Helper:
+    def __init__(self, localizableFilename):
+        self.localizableFilename = localizableFilename
         self.strings_in_localizable = {}
 
     def get_localizable_strings_apple(self):
-        with open(self.filename,'r') as stringFile:
+        with open(self.localizableFilename, 'r') as stringFile:
             buffer = stringFile.read()
             buffer = buffer.replace('\n', '')
             pairs = buffer.split(';')
@@ -31,6 +32,7 @@ class Parser:
 
     def find_unused_localized_strings(self, directory, source_extensions):
         count = 0
+        iteration = 0
         for string in self.strings_in_localizable:
             list_of_source_files = self.gather_source_files(directory, source_extensions)
             stringCount = 0
@@ -40,8 +42,38 @@ class Parser:
                     matches = re.findall(string, source_text)
                     stringCount += len(matches)
 
+
             if stringCount == 0:
                 print("String " + string + " can be removed")
                 count += 1
 
+            iteration += 1
+            printProgressBar(iteration, len(self.strings_in_localizable))
+
         print(str(count) + " strings can be removed")
+
+    def purge_localizable_file(self, removableKeysFilename, output="output.strings"):
+        removableKeysFile = open(removableKeysFilename, 'r')
+        localizableFile = open(self.localizableFilename, 'r')
+        outputFile = open(output, 'w')
+
+        iteration = 0
+        totalIterations = sum(1 for line in localizableFile)
+        localizableFile.seek(0, 0)
+        for line in localizableFile:
+            printProgressBar(iteration, totalIterations)
+            if not self.is_line_to_be_removed(line, removableKeysFile):
+                outputFile.write(line)
+            iteration += 1
+
+        removableKeysFile.close()
+        outputFile.close()
+        localizableFile.close()
+
+    def is_line_to_be_removed(self, line, keys):
+        keys.seek(0, 0)
+        for key in keys:
+            if re.match(key.rstrip(), line) is not None:
+                return True
+
+        return False
--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
-from parser import Parser
+from helper import Helper
 import sys
 if __name__ == '__main__':
 
-    if len(sys.argv) < 3:
-        print("Usage: python main.py <name of Localizable.strings file> <name of project root> <...extensions to search in...>")
+    if sys.argv[1] == "--dry-run":
+        helper = Helper(sys.argv[2])
+        helper.get_localizable_strings_apple()
+        helper.find_unused_localized_strings(sys.argv[3], sys.argv[4:])
+
+    elif sys.argv[1] == "--purge":
+        helper = Helper(sys.argv[2])
+        helper.purge_localizable_file(sys.argv[3], output=sys.argv[4])
+
+    else:
+        print("""
+        Usage: python main.py --dry-run <name of Localizable.strings file> <name of project root> <...extensions to search in...>
+        or
+        python main.py --purge <name of strings file> <name of file of strings to be removed> <output filename>
+        """)
         exit(1)
 
-    parser = Parser(sys.argv[1])
-    parser.get_localizable_strings_apple()
-    parser.find_unused_localized_strings(sys.argv[2], sys.argv[3:])
+
